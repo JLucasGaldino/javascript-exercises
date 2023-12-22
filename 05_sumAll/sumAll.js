f68b5dc0b2534e2dcce9bb38b1f3d543010798be
JLucasGaldino <joaolngaldino@gmail.com>
2023-12-22 16:06:04 +0000

    Complete up to 5th exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(number1, number2) {
+    //sort arguments alphanumerically
+    let argumentArray = [number1, number2];
+    argumentArray.sort();
+    let min = argumentArray[0];
+    let max = argumentArray[1];
+    //return error for negative minimum
+    if (min < 0) {
+        return 'ERROR';
+        //return error for non-numerical values
+    }else if (typeof(min) !== "number" || typeof(max) !== "number"){
+        return 'ERROR';
+    } else {
+        //calculate sum
+        let totalSum = 0;
+        for (i = min; i <= max; i++) {
+            totalSum += i;
+        }
+        return totalSum;
+    }
 };
 
 // Do not edit below this line
