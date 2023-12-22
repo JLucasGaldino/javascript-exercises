8139632fcebd6e8d553165f8df9769693c7f4bc5
JLucasGaldino <joaolngaldino@gmail.com>
2023-12-22 16:30:27 +0000

    Complete to the 6th exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitOriginal) {
+  let unroundedConvertedToCelsius = ((fahrenheitOriginal - 32) * 5 / 9);
+  return Math.round(unroundedConvertedToCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusOriginal) {
+  let unroundedCOnvertedToFahrenheit = (celsiusOriginal * 9 / 5 + 32);
+  return Math.round(unroundedCOnvertedToFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
