dbce56b4c4e7872fac76e10c90f61f230c486ebe
JLucasGaldino <joaolngaldino@gmail.com>
2023-12-22 11:04:19 +0000

    Complete up to the 4th exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split("");
+    const reversedArray = stringArray.reverse();
+    let reversedString = "";
+    for (let character of reversedArray) {
+        reversedString += character;
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
